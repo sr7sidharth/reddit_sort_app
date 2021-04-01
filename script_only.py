021dfe93aec79b355ef96dfdefdb9e110c6c4328
sr7sidharth <45019932+sr7sidharth@users.noreply.github.com>
2021-04-01 01:05:23 -0700

    Update script_only.py

--- script_only.py
+++ script_only.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
 	mdFile = MdUtils(file_name="results"+str(sys.argv[3]), title="Found results for: " + str(sys.argv[3]))
 
-	for item in reddit.user.me().saved(limit=int(sys.argv[4])):
+	for item in reddit.user.me().saved(limit=int(sys.argv[4]) if len(sys.argv) == 5 else None):
 		if isinstance(item,praw.models.Submission):
 			subreddit = item.subreddit
 			if subreddit.display_name == str(sys.argv[3]):
