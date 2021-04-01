cd80fa21ce3e04aca7dc19711ec2c648681adf12
sr7sidharth <45019932+sr7sidharth@users.noreply.github.com>
2021-04-01 00:15:54 -0700

    Basic functionality complete

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ reddit = praw.Reddit(
 
 @app.route('/')
 def main():
-	url = reddit.auth.url(["identity, history"], "...", "permanent")
+	url = reddit.auth.url(["*"], "...", "permanent")
 	text = '<a href="%s")>Click here to authorize reddit</a>'
 	return text % url
 
@@ -29,7 +29,10 @@ def sorted():
 	code = request.args.get('code')
 	reddit.auth.authorize(code)
 	text = ""
-	for item in reddit.user.me().saved(limit=1000):
-		text += '<a href="' + str(item.url) + '">'+ str(item.title) + '</a>'
-		text += "<br>"
+	for item in reddit.user.me().saved(limit=50):
+		if isinstance(item,praw.models.Submission):
+			subreddit = item.subreddit
+			if subreddit.display_name == "thefalconandthews":
+				text += '<a href="' + str(item.url) + '">'+ str(item.title) + '</a>'
+				text += "<br>"
 	return text
